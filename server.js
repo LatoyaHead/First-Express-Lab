200b42ae1beb45d3e463cf0b5fb7de99c779af90
24HourDeveloper <latoya30310@gmail.com>
2022-09-29 18:18:03 -0400

    Tip calculator express application created.

--- server.js
+++ server.js
@@ -12,7 +12,20 @@ app.get('/greeting/:name', (req, res) => {
   res.send(`Hello, ${name}! It's so great to see you!`)
 })
 
+//Tip Calculator
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+  const {total, tipPercentage} = req.params
+  let tip = (tipPercentage / 200) * total
+  res.send(`Tip: ${tip}`)
+})
 
+//Magic 8 Ball
+app.get('/magic/:question', (req, res) => {
+  const {question} = req.params
+  const ballResponse = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+  let index = Math.floor(Math.random() * ballResponse.length())
+  res.send(`<h1>${question} ${ballResponse[index]}</h1>`)
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
